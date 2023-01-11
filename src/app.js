05a5179d9d202e503a3ca4d0750ce4ebd073e59a
bedulem <santochibelen@gmail.com>
2023-01-11 16:12:31 -0300

    add info

--- src/app.js
+++ src/app.js
@@ -9,7 +9,13 @@ app.use(express.json());
 app.use(inedexRoutes);
 app.use("/api", heroCardsRoutes);
 app.get("/", (req, res) => {
-  res.send("Hello Word");
+  res.send(`
+  <h1>Hero Cards</h1>
+  <p>----------------------<p>
+  <p>Get information from Hero Realms cards with this API</p>
+  <h3>GET -  api/herocards</h3>
+  <h3>GET -  api/herocards/:id</h3>
+  <h3>GET - /ping`);
 });
 
 app.use((req, res, next) => {
