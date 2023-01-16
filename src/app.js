32835c040dbf1901ce969cfc04b5c45ddc1aabe4
bedulem <santochibelen@gmail.com>
2023-01-16 18:18:18 -0300

    enable all cors request

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import inedexRoutes from "./routes/index.routes.js";
 import heroCardsRoutes from "./routes/heroCards.routes.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(inedexRoutes);
